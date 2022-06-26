dd2d99f74649e023cd9774e7e4a58291c3742814
Yash Goyal <yashgoyal1620@gmail.com>
2022-06-26 10:32:14 +0530

    workday8 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:catalog_app/model/catalog.dart';
 import 'package:catalog_app/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/item_widgets.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
@@ -7,16 +10,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
         centerTitle: true,
       ),
-      body: Center(
-          child: Container(
-        child: Text("Welcome $days $name"),
-      )),
-      // drawer: MyDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
+      ),
       drawer: MyDrawer(),
     );
   }
